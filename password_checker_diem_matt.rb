6298f18b6b245cb1ace1636a71b2a92b484a203d
DiemBTran <diembtran@gmail.com>
2020-09-22 16:47:03 -0700

    we're having a weird isssue with the password checker. the bang operator and the or operators are not working well together for the elsif

---
+++ password_checker_diem_matt.rb
@@ -0,0 +1,17 @@
+def password_checker (user_id, password)
+    if user_id == password
+        'Your user ID and password cannot be the same'
+    elsif user_id.length < 6 || password.length < 6
+        'Both your user ID and password must be over 6 characters long'
+    elsif password == "password"
+        "Your password cannot be 'password'"
+    elsif !password.include?'!' and !password.include?'#' and !password.include?'$'
+        "Your password needs to include a '!', '#', or '$'."
+    elsif user_id.include?'!' or user_id.include?'#' or user_id.include?'$' 
+        "Your user ID cannot include a '!', '#', or '$'."
+    else
+        'Thanks for signing up!'
+    end
+end
+
+p password_checker 'matteee', 'wordpass#'
--- ruby_conditionals_diem_matt.rb
+++ ruby_conditionals_diem_matt.rb
@@ -1,23 +1,24 @@
 p 'Player 1 choose rock, paper, or scissors:'
-player_1_answer = gets.downcase
-
+player_1_answer = gets.downcase.chomp
+p player_1_answer
 p 'Player 2 choose rock, paper, or scissors:'
- player_2_answer = gets.downcase
+ player_2_answer = gets.downcase.chomp
+p player_2_answer
 
 if player_1_answer == player_2_answer
-    p "#{player_1_answer} and #{player_2_answer} are draw"
+    p "#{player_1_answer} and #{player_2_answer} are draw. Player 1 wins!"
 elsif player_1_answer == "rock" && player_2_answer == "scissors"
-    p "#{player_1_answer} beats #{player_2_answer}"
+    p "#{player_1_answer} beats #{player_2_answer}. Player 1 wins!"
 elsif player_1_answer == "paper" && player_2_answer == "rock"
-    p "#{player_1_answer} beats #{player_2_answer}"
+    p "#{player_1_answer} beats #{player_2_answer}. Player 1 wins!"
 elsif player_1_answer == "scissors" && player_2_answer == "paper"
-    p "#{player_1_answer} beats #{player_2_answer}"
+    p "#{player_1_answer} beats #{player_2_answer}. Player 1 wins!"
 elsif player_2_answer == "rock" && player_1_answer == "scissors"
-    p "#{player_2_answer} beats #{player_1_answer}"
+    p "#{player_2_answer} beats #{player_1_answer}. Player 2 wins!"
 elsif player_2_answer == "paper" && player_1_answer == "rock"
-    p "#{player_2_answer} beats #{player_1_answer}"
+    p "#{player_2_answer} beats #{player_1_answer}. Player 2 wins!"
 elsif player_2_answer == "scissors" && player_1_answer == "paper"
-    p "#{player_2_answer} beats #{player_1_answer}"
+    p "#{player_2_answer} beats #{player_1_answer}. Player 2 wins!"
 else 
     p 'Please do something'
 end
