8b2d2a2c47d6fde007cba355e4214f0667ab4d4d
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-22 17:31:36 -0700

    fixed logic in pswd checker

--- ruby_methods_chuck_david.rb
+++ ruby_methods_chuck_david.rb
@@ -27,56 +27,67 @@ def is_palindrome string
 end
 
 def password_checker (id, password)
-  valid_user_input = 'NO'
+  valid_user_input = 'YES'
+  err_index = nil
+  error_msg_array = ["ID and PSWD cannot be the same", "ID must be > 6 chars", "PSWD must be > 6 chars", "ID cannot contain blank spaces", "PSWD cannot be Password", "PSWD must have: #, !, or $", "ID cannot have: #, !, or $"]
 
   if id == password
-    'ID and Password cannot be the same!'
+    # 'ID and Password cannot be the same!'
+    valid_user_input = 'NO'
+    err_index = 0
   elsif id.length < 6
-    'ID must be greater than 6 characters!'
+    # 'ID must be greater than 6 characters!'
+    valid_user_input = 'NO'
+    err_index = 1
   elsif password.length < 6
-    'Password must be greater than 6 characters!'
+    # 'Password must be greater than 6 characters!'
+    valid_user_input = 'NO'
+    err_index = 2
   elsif id.include? " "
-    'User ID must not contain any blank spaces!'
+    # 'User ID must not contain any blank spaces!'
+    valid_user_input = 'NO'
+    err_index = 3
+  # 'Password can NOT be Password!'
   elsif password.downcase == 'password'
-    'Password can NOT be Password!'
-  end
-
-  if password.include? "#"
+    valid_user_input = 'NO'
+    err_index = 4
+  # password must include one of these
+  elsif password.include? "#"
     valid_user_input = 'YES'
+    err_index = 5
   elsif password.include? "!"
     valid_user_input = 'YES'
+    err_index = 5
   elsif password.include? "$"
     valid_user_input = 'YES'
-  else
-    'Password must include one of these characters: #, !, $'
-  end
-
-  if id.include? "#"
+    err_index = 5
+  # ID cannot conatain these characters
+  elsif id.include? "#"
     valid_user_input = 'NO'
-    'ID must NOT include one of these characters: #, !, $'
+    err_index = 6
   elsif id.include? "!"
     valid_user_input = 'NO'
-    'ID must NOT include one of these characters: #, !, $'
+    err_index = 6
   elsif id.include? "$"
     valid_user_input = 'NO'
-    'ID must NOT include one of these characters: #, !, $'
-  else
-    valid_user_input = 'YES'
+    err_index = 6
   end
 
   if valid_user_input == 'YES'
     'ID and Password are Valid, thanks!'
+  else
+    "ID and Password are NOT Valid, #{error_msg_array[err_index]}"
   end
 end
 
-puts sum_these_numbers 5,10
-puts is_even 10
-puts within_ten 20
-puts is_palindrome 'racecar'
+# puts sum_these_numbers 5,10
+# puts is_even 10
+# puts within_ten 20
+# puts is_palindrome 'racecar'
 puts password_checker 'watermelon', 'watermelon'
-# puts password_checker 'water', 'watermelon'
-# puts password_checker 'watermelon', 'water'
-# puts password_checker 'water melon', 'watermelons'
-# puts password_checker 'watermelon', 'Password'
-puts password_checker 'watermelon', 'watermelon'
-# puts password_checker 'watermelon', 'watermelon'
+puts password_checker 'water', 'watermelon'
+puts password_checker 'watermelon', 'water'
+puts password_checker 'water melon', 'watermelons'
+puts password_checker 'watermelon', 'Password'
+puts password_checker 'watermelon$', 'watermelon$'
+puts password_checker 'watermelon', 'watermelon$'
