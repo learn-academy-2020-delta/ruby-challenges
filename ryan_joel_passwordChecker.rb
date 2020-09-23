b815b9f38414e6d8d28f479d17db94e1ac062e41
ryanizumi16 <ryanizumi16@gmail.com>
2020-09-23 11:27:59 -0700

    finished?

--- ryan_joel_passwordChecker.rb
+++ ryan_joel_passwordChecker.rb
@@ -15,13 +15,35 @@
 
 # puts is_even 6
 
-def ticket number
-    if number <= 10
-        'VALID'
-    else
-        'Invalid'
-    end
+# def ticket number
+#     if number <= 10
+#         'VALID'
+#     else
+#         'Invalid'
+#     end
+# end
+#
+# puts ticket 1
+
+p 'user_id here'
+user_id = gets.chomp
+p 'password here'
+password = gets.chomp
+
+def password_checker (user_id, password)
+  if user_id == password
+    'user_id and password can NOT be the same'
+  elsif user_id.length < 6
+    'user_id must contain atleast 6 characters'
+  elsif password.length < 6
+    'password must contain atleast 6 characters'
+  elsif password == 'password'
+    "#{password} can Not be password"
+  elsif user_id.include?  "!" || "#" || "$"
+    'user_id can NOT contain !, #, or $'
+  elsif password.include? "!" || "#" || "$"
+  else 'password needs to include !, #, or $'
+  end
 end
 
-puts ticket 1
-
+puts password_checker user_id, password
