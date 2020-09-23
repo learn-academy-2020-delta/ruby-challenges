6b1122a301d19f58a6b6824cbcf4459317ad4b71
LEARN Academy <devjoelmichael@protonmail.com>
2020-09-23 10:48:28 -0700

    finished challenges

--- ruby_blocks_chuck_joel.rb
+++ ruby_blocks_chuck_joel.rb
@@ -39,20 +39,67 @@
 #
 # to_caps(array_of_words)
 
-array_of_nums = [1, 2, 3, 4, 5, 200, 6]
+# array_of_nums = [1, 2, 3, 4, 5, 200, 6]
 
-def largest_num (array)
-  prev_num = 0
-  current_num = 0
-  high_num = 0
-  array.map do |value|
-    current_num = value
-    if current_num > prev_num
-      high_num = current_num
+# def largest_num (array)
+#   prev_num = 0
+#   current_num = 0
+#   high_num = 0
+#   array.map do |value|
+#     current_num = value
+#     if current_num > prev_num
+#       high_num = current_num
+#     end
+#     prev_num = current_num
+#   end
+#   p high_num
+# end
+
+# largest_num(array_of_nums)
+
+# def smallest_num (arr)
+#   p arr.min
+# end
+
+# smallest_num(array_of_nums)
+
+# my_array = [7, 1, 2, 3, 4, 5, 6]
+
+# def is_odd (arr)
+#   new_array = []
+#   arr.map do |value| 
+#     if value % 2 == 1
+#       new_array<<value
+#     end
+#   end
+#   p new_array.sort()
+# end
+
+# is_odd(my_array)
+
+array_string = ['Apple', 'Potato', 'Green', 'Home', 'Red']
+
+# def rev_case (arr)
+#   new_array = []
+#   arr.map do |value|
+#     new_array<<value.swapcase()
+#   end
+#   p new_array
+# end 
+
+# rev_case(array_string)
+
+def letter_case (arr, letter)
+  new_array = []
+  arr.map do |value|
+    if value[0] == letter.upcase() || value[0] == letter.downcase()
+      new_array<<value
     end
-    prev_num = current_num
-  end
-  p high_num
+  end  
+  p new_array
 end
 
-largest_num(array_of_nums)
+letter_case(array_string, 'P')
+
+
+
